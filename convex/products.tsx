61ebd3483fbff3a628e50c9e9965e57ab6ce1ebd
mustafa-hanif <mustafa.hanif@seera.sa>
2023-10-29 20:08:05 +0400

    init buy local

---
+++ convex/products.tsx
@@ -0,0 +1,22 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const get = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("products").collect();
+  },
+});
+
+export const add = mutation({
+  args: { 
+    name: v.string(),
+    country: v.string(),
+    type: v.string(),
+    alternate: v.optional(v.id("products")),
+  },
+  handler: async (ctx, { name, country, type, alternate }) => {
+    // Send a new message.
+    await ctx.db.insert("products", { name, country, type, alternate });
+  },
+});
